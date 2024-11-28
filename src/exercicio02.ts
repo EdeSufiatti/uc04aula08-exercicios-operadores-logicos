3325709478099c7433acd5e6719428feeb2811e4
Ede Sufiatti <sufiattie@gmail.com>
2024-11-28 20:48:55 -0300

    4c

--- src/exercicio02.ts
+++ src/exercicio02.ts
@@ -1,1 +1,10 @@
 //2. Utilizando (DoWhile), elaborar um programa que apresente o somatório dos valores pares existentes na faixa de 1 até 500.
+
+let soma = 0;
+let numero = 2;
+
+do {
+    soma += numero;
+    numero += 2;
+} while (numero <= 500);
+console.log(soma)
