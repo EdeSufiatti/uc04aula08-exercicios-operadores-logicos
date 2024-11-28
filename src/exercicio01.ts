8b54c7cfa40d0014ff7e0abe65ba9b2e71a640dc
Ede Sufiatti <sufiattie@gmail.com>
2024-11-28 20:00:41 -0300

    3c

--- src/exercicio01.ts
+++ src/exercicio01.ts
@@ -1,3 +1,12 @@
 
 //    1. Utilizando (While), construir um programa que apresente a soma dos cem primeiros números naturais (1 + 2 + 3 +...+ 98 + 99 + 100).
 
+
+// Declaração do array numerico
+let somaNaturais = 0;
+let i = 0;
+
+while (i <= 100) {
+  somaNaturais =
+}
+
