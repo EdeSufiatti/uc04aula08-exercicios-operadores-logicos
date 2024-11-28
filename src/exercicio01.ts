3a48dbad6f98a49298ea298db77a0ad34a2e4f01
Ede Sufiatti <sufiattie@gmail.com>
2024-11-28 19:55:58 -0300

    to commit

---
+++ src/exercicio01.ts
@@ -0,0 +1,3 @@
+
+//    1. Utilizando (While), construir um programa que apresente a soma dos cem primeiros números naturais (1 + 2 + 3 +...+ 98 + 99 + 100).
+
---
+++ src/exercicio02.ts
@@ -0,0 +1,1 @@
+//2. Utilizando (DoWhile), elaborar um programa que apresente o somatório dos valores pares existentes na faixa de 1 até 500.
